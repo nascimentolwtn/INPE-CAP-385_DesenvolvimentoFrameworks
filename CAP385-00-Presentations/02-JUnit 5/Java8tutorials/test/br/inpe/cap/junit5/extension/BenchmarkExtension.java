e6067202908410002be5c62e7a937f2311d0ac84
VANT <VANT@VANT-PC>
2016-07-17 23:48:28 -0300

    Inclusão de anotações para apresentação do ponto de extensão de um framework.

---
+++ CAP385-00-Presentations/02-JUnit 5/Java8tutorials/test/br/inpe/cap/junit5/extension/BenchmarkExtension.java
@@ -0,0 +1,76 @@
+package br.inpe.cap.junit5.extension;
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ContainerExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.TestExtensionContext;
+
+import static java.lang.System.currentTimeMillis;
+
+public class BenchmarkExtension
+		implements BeforeAllCallback, BeforeEachCallback, AfterEachCallback, AfterAllCallback {
+
+	private static final Namespace NAMESPACE = Namespace.create("BenchmarkExtension");
+
+	@Override
+	public void beforeAll(ContainerExtensionContext context) {
+		if (!shouldBeBenchmarked(context))
+			return;
+
+		writeCurrentTime(context, LaunchTimeKey.CLASS);
+	}
+
+	@Override
+	public void beforeEach(TestExtensionContext context) {
+		if (!shouldBeBenchmarked(context))
+			return;
+
+		writeCurrentTime(context, LaunchTimeKey.TEST);
+	}
+
+	@Override
+	public void afterEach(TestExtensionContext context) {
+		if (!shouldBeBenchmarked(context))
+			return;
+
+		long launchTime = loadLaunchTime(context, LaunchTimeKey.TEST);
+		long runtime = currentTimeMillis() - launchTime;
+		print("Test", context.getDisplayName(), runtime);
+	}
+
+	@Override
+	public void afterAll(ContainerExtensionContext context) {
+		if (!shouldBeBenchmarked(context))
+			return;
+
+		long launchTime = loadLaunchTime(context, LaunchTimeKey.CLASS);
+		long runtime = currentTimeMillis() - launchTime;
+		print("Test container", context.getDisplayName(), runtime);
+	}
+
+	private static boolean shouldBeBenchmarked(ExtensionContext context) {
+		return context.getElement()
+				.map(el -> el.isAnnotationPresent(Benchmark.class))
+				.orElse(false);
+	}
+
+	private static void writeCurrentTime(ExtensionContext context, LaunchTimeKey key) {
+		context.getStore(NAMESPACE).put(key, currentTimeMillis());
+	}
+
+	private static long loadLaunchTime(ExtensionContext context, LaunchTimeKey key) {
+		return context.getStore(NAMESPACE).remove(key, long.class);
+	}
+
+	private static void print(String unit, String displayName, long runtime) {
+		System.out.printf("%s '%s' took %d ms.%n", unit, displayName, runtime);
+	}
+
+	private enum LaunchTimeKey {
+		CLASS, TEST
+	}
+}
