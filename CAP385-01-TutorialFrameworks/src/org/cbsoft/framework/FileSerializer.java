196b61117f42a364ced10ccab589d054581bf8f9
Luiz Wagner <LuizWagner@Aline-SSHD>
2016-07-22 00:02:43 -0300

    Implementação da refatoração de frameworks - Parte 2: - Remoção da interface "intrusiva" de domínio PropertiesGetter - Utilização de Reflection para obtenção da responsabilidade anterior da PropertiesGetter - Inclusão de @Anottation para marcar métodos que não devem ser considerados automaticamente pelo processamento por reflection do FileSerializer

--- CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/FileSerializer.java
+++ CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/FileSerializer.java
@@ -1,6 +1,9 @@
 package org.cbsoft.framework;
 
 import java.io.FileOutputStream;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileSerializer {
 
@@ -13,8 +16,8 @@ public class FileSerializer {
 		this.dataFormatter = dataFormatter;
 	}
 
-	public void generateFile(String filename, PropertiesGetter propGetter) {
-		byte[] bytes = this.dataFormatter.formatData(propGetter.getPropertiesList());
+	public void generateFile(String filename, Object propGetter) {
+		byte[] bytes = this.dataFormatter.formatData(this.getPropertiesList(propGetter));
 		
 		try {
 			bytes = this.postProcessor.postProcess(bytes);	
@@ -26,4 +29,33 @@ public class FileSerializer {
 		}
 	}
 	
+	private Map<String, Object> getPropertiesList(Object obj){
+		Map<String, Object> props = new HashMap<String, Object>();
+		Class<?> clazz = obj.getClass();
+		for (Method m : clazz.getMethods()) {
+			if(isProcessableGetterMethod(m)) {
+				
+				try {
+					Object value = m.invoke(obj);
+					String getterName = m.getName();
+					String propName = getterName.substring(3, 4).toLowerCase() + getterName.substring(4);
+					
+					props.put(propName, value);
+				} catch (Exception e) {
+					throw new RuntimeException("Cannot retrieve properties", e);
+				}
+				
+			}
+		}
+		
+		return props;
+	}
+
+	private boolean isProcessableGetterMethod(Method m) {
+		return m.getName().startsWith("get") && 
+		   m.getParameterTypes().length == 0 &&
+		   m.getReturnType() != void.class &&
+		   !m.getName().equals("getClass") &&
+		   !m.isAnnotationPresent(DontIncludeOnFile.class);
+	}
 }
