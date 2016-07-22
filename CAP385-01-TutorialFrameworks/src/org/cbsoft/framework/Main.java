1f8a6b2a4af817e370f2222ca3c872cf4d6f719b
Luiz Wagner <LuizWagner@Aline-SSHD>
2016-07-22 00:02:43 -0300

    Implementação da refatoração de frameworks - Parte 3: - Remoção da interface "intrusiva" de domínio PropertiesGetter - Utilização de Reflection para obtenção da responsabilidade anterior da PropertiesGetter - Inclusão de @Anottation para marcar métodos que não devem ser considerados automaticamente pelo processamento por reflection do FileSerializer

--- CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/Main.java
+++ CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	
 	public static void main(String[] args){
 		Product p = new Product("notebook", "HP", 1999.99, "2348203894032948");
+		p.setSecretCode("não deve aparecer!");
 		
 		FileSerializer cxs = new FileSerializer(new Compressor(), new XMLFormatter());
 		cxs.generateFile("product.zip", p);
--- CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/Product.java
+++ CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/Product.java
@@ -1,14 +1,12 @@
 package org.cbsoft.framework;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Product implements PropertiesGetter{
+public class Product {
 	
 	private String name;
 	private String brand;
 	private double price;
 	private String code;
+	private String secretCode;
 	
 	public Product(String name, String brand, double price, String code) {
 		this.name = name;
@@ -16,6 +14,7 @@ public class Product implements PropertiesGetter{
 		this.price = price;
 		this.code = code;
 	}
+	
 	public String getName() {
 		return name;
 	}
@@ -40,16 +39,12 @@ public class Product implements PropertiesGetter{
 	public void setCode(String code) {
 		this.code = code;
 	}
-	@Override
-	public Map<String, Object> getPropertiesList() {
-		Map<String,Object> props = new HashMap<String, Object>();
-		props.put("name", name);
-		props.put("brand", brand);
-		props.put("price", price);
-		props.put("code", code);
-		return props;
+	@DontIncludeOnFile
+	public String getSecretCode() {
+		return secretCode;
 	}
-	
-	
+	public void setSecretCode(String secretCode) {
+		this.secretCode = secretCode;
+	}
 
 }
