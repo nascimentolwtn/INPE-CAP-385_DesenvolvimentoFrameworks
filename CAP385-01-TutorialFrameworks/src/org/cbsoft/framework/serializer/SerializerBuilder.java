168fd00ac1948735661c1b828d8b5fbe347bbb9a
Luiz Wagner <nascimentolwtn@gmail.com>
2016-08-05 00:50:16 -0300

    Criação de classe interna para esconder métodos de criação, disponibilizando-os apenas após chamada única de createXML/Properties.

--- CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/serializer/SerializerBuilder.java
+++ CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/serializer/SerializerBuilder.java
@@ -11,47 +11,53 @@ import org.cbsoft.framework.processor.PostProcessor;
 public class SerializerBuilder {
 	
 	private Serializer serializer;
-	
-	public Serializer build() {
-		return this.serializer;
-	}
 
-	public SerializerBuilder createXMLSerializer() {
-		serializer = new FileSerializer(new NullPostProcessor(), new XMLFormatter());
-		return this;
+	public SerializerInConstruction createXMLSerializer() {
+		return new SerializerInConstruction(new FileSerializer(new NullPostProcessor(), new XMLFormatter()));
 	}
 	
-	public SerializerBuilder createPropertiesSerializer() {
-		serializer = new FileSerializer(new NullPostProcessor(), new PropertiesFormatter());
-		return this;
+	public SerializerInConstruction createPropertiesSerializer() {
+		return new SerializerInConstruction(new FileSerializer(new NullPostProcessor(), new PropertiesFormatter()));
 	}
 	
-	public SerializerBuilder withEncription(int number) {
-		PostProcessor pp = new Crypto(number);
-		this.addPostProcessor(pp);
-		return this;
-	}
-	
-	private void addPostProcessor(PostProcessor postProcessor) {
-		PostProcessor current = serializer.getPostProcessor();
-		if(current instanceof NullPostProcessor) {
-			serializer.setPostProcessor(postProcessor);
-		} else {
-			CompositePostProcessor composite =
-					new CompositePostProcessor(current, postProcessor);
-			this.serializer.setPostProcessor(composite);
+	public class SerializerInConstruction {
+		
+		public SerializerInConstruction(Serializer serializer) {
+			SerializerBuilder.this.serializer = serializer;
 		}
-	}
-	
-	public SerializerBuilder withLogging() {
-		this.serializer = new SerializerLogger(this.serializer);
-		return this;
-	}
-	
-	public SerializerBuilder withCompression() {
-		PostProcessor pp = new Compressor();
-		this.addPostProcessor(pp);
-		return this;
+		
+		public SerializerInConstruction withEncription(int number) {
+			PostProcessor pp = new Crypto(number);
+			this.addPostProcessor(pp);
+			return this;
+		}
+		
+		private void addPostProcessor(PostProcessor postProcessor) {
+			PostProcessor current = serializer.getPostProcessor();
+			if(current instanceof NullPostProcessor) {
+				serializer.setPostProcessor(postProcessor);
+			} else {
+				CompositePostProcessor composite =
+						new CompositePostProcessor(current, postProcessor);
+				SerializerBuilder.this.serializer.setPostProcessor(composite);
+			}
+		}
+		
+		public SerializerInConstruction withLogging() {
+			SerializerBuilder.this.serializer = new SerializerLogger(SerializerBuilder.this.serializer);
+			return this;
+		}
+		
+		public SerializerInConstruction withCompression() {
+			PostProcessor pp = new Compressor();
+			this.addPostProcessor(pp);
+			return this;
+		}
+		
+		public Serializer build() {
+			return SerializerBuilder.this.serializer;
+		}
+
 	}
 	
 }
